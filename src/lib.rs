ea2d87ca8ae2716ef7f9117513cde806d78dda8a
Martin Junghanns <martin.junghanns@neo4j.com>
2020-12-11 18:28:42 +0100

    Add dequeue operation
    Co-authored-by: Paul Horn <paul.horn@neotechnology.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 use core::ptr;
 use std::mem::MaybeUninit;
-use std::rc::Rc;
-use std::sync::Arc;
 use std::sync::atomic::{AtomicPtr, AtomicU8, AtomicUsize, Ordering};
+use std::sync::atomic::Ordering::{Acquire, Release};
 
-use crate::State::Empty;
+use crate::State::{Empty, Set};
 
 // NODE_SIZE
 const BUFFER_SIZE: usize = 1620;
@@ -184,6 +183,174 @@ impl<T> MpscQueue<T> {
             }
         }
     }
+
+    fn dequeue(&mut self) -> Option<T> {
+        let mut temp_tail;
+
+        loop {
+            temp_tail = unsafe { &mut *self.tail_of_queue.load(Ordering::Acquire) };
+            // the amount of items in the queue without the current buffer
+            let mut prev_size = self.buffer_size * ((*temp_tail).position_in_queue - 1);
+
+            let head = &mut unsafe { &mut *self.head_of_queue }.head;
+            let head_is_tail = self.head_of_queue == self.tail_of_queue.load(Ordering::Acquire);
+            let head_is_empty = *head == self.tail.load(Acquire) - prev_size;
+
+            if head_is_tail && head_is_empty {
+                // queue is empty
+                break None;
+            } else if *head < self.buffer_size {
+                // there are elements in the current buffer
+                let node = &mut unsafe { &mut *self.head_of_queue }.curr_buffer[*head];
+
+                if node.is_set.load(Acquire) == State::Handled {
+                    *head += 1;
+                    continue;
+                }
+
+                let mut temp_head_of_queue = self.head_of_queue;
+                let mut temp_head = *head;
+                let mut flag_move_to_new_buffer = false;
+                let mut flag_buffer_all_handled = true;
+
+                while node.is_set.load(Acquire) == State::Empty {
+                    if temp_head < self.buffer_size {
+                        // there are elements in the current buffer
+                        let mut temp_node = &mut unsafe { &mut *self.head_of_queue }.curr_buffer[temp_head];
+                        temp_head += 1;
+
+                        if temp_node.is_set.load(Acquire) == State::Set && node.is_set.load(Acquire) == State::Empty {
+                            // the data is valid and the element in the head is still in enqueue process
+                            let mut scan_head_of_queue = self.head_of_queue;
+
+                            let mut scan_head = unsafe { &*scan_head_of_queue }.head;
+
+                            // start scanning
+                            while scan_head_of_queue != temp_head_of_queue || scan_head < (temp_head - 1) && node.is_set.load(Acquire) == State::Empty {
+                                if scan_head > self.buffer_size {
+                                    // end of buffer
+                                    // go to next buffer
+                                    scan_head_of_queue = unsafe { (*scan_head_of_queue).next.load(Acquire) };
+                                    scan_head = unsafe { (*scan_head_of_queue).head };
+                                    continue;
+                                }
+
+                                let scan_node = &mut unsafe { &mut *scan_head_of_queue }.curr_buffer[scan_head];
+
+                                if scan_node.is_set.load(Acquire) == State::Set {
+                                    // there is another element that is set
+                                    // this is the new item to be evicted
+                                    temp_head = scan_head;
+                                    temp_head_of_queue = scan_head_of_queue;
+                                    temp_node = scan_node;
+
+                                    // re-scan from the beginning of the queue
+                                    scan_head_of_queue = self.head_of_queue;
+                                    scan_head = unsafe { &*scan_head_of_queue }.head;
+                                }
+
+                                scan_head += 1;
+                            }
+
+                            if node.is_set.load(Acquire) == State::Set {
+                                break;
+                            }
+
+                            // end scanning
+
+                            let data = unsafe { temp_node.data.as_ptr().read() };
+                            // idea is to avoid double-free once buffer is dropped (and the consumer dropped T before)
+                            temp_node.data = MaybeUninit::uninit();
+                            temp_node.is_set.store(State::Handled.into(), Release);
+
+                            if flag_move_to_new_buffer && (temp_head - 1) == unsafe { (*temp_head_of_queue).head } {
+                                // if we moved to a new buffer, we need to move the head forward so in the end we can delete the buffer
+                                unsafe { (*temp_head_of_queue).head += 1 };
+                            }
+
+                            Some(data);
+                        }
+
+                        if temp_node.is_set.load(Acquire) == Empty {
+                            flag_buffer_all_handled = false;
+                        }
+                    }
+
+                    if temp_head >= self.buffer_size {
+                        // we reach the end of the buffer - move to the next
+                        if flag_buffer_all_handled && flag_move_to_new_buffer {
+                            // we want to "fold" the queue
+                            // if there is a thread that got stuck - we want to keep only that buffer and delete the rest (before it)
+
+                            if temp_head_of_queue == self.tail_of_queue.load(Acquire) {
+                                // there is no place to move
+                                return None;
+                            }
+
+                            let next = unsafe { &*temp_head_of_queue }.next.load(Acquire);
+                            let prev = unsafe { &*temp_head_of_queue }.prev;
+
+                            if next == ptr::null_mut() {
+                                // if we do not have where to move ? what ?
+                                return None;
+                            }
+
+                            unsafe { (*next).prev = prev };
+                            unsafe { (*prev).next.store(next, Ordering::Release) };
+
+                            // drop folded buffer
+                            drop(unsafe { Box::from_raw(temp_head_of_queue) });
+
+                            temp_head_of_queue = next;
+                            temp_head = unsafe { &*temp_head_of_queue }.head;
+                            flag_buffer_all_handled = true;
+                            flag_move_to_new_buffer = true;
+                        } else {
+                            let next = unsafe { &*temp_head_of_queue }.next.load(Acquire);
+                            if next == ptr::null_mut() {
+                                // if we do not have where to move ? what ?
+                                return None;
+                            }
+                            temp_head_of_queue = next;
+                            temp_head = unsafe { &*temp_head_of_queue }.head;
+                            flag_buffer_all_handled = true;
+                            flag_move_to_new_buffer = true;
+                        }
+                    }
+                }
+
+                // try to take out set elements that are next in line
+                if node.is_set.load(Acquire) == Set {
+                    // valid
+                    unsafe { (*self.head_of_queue).head += 1 };
+
+                    let data = unsafe { node.data.as_ptr().read() };
+                    // idea is to avoid double-free once buffer is dropped (and the consumer dropped T before)
+                    node.data = MaybeUninit::uninit();
+                    node.is_set.store(State::Handled.into(), Release);
+
+                    return Some(data);
+                }
+            }
+            if unsafe { (*self.head_of_queue).head } >= self.buffer_size {
+                // move to next array and the delete the prev array
+                if self.head_of_queue == self.tail_of_queue.load(Acquire) {
+                    // there is no place to move
+                    return None;
+                }
+
+                let next = unsafe { &*self.head_of_queue }.next.load(Acquire);
+                if next == ptr::null_mut() {
+                    return None;
+                }
+
+                // drop folded buffer
+                drop(unsafe { Box::from_raw(self.head_of_queue) });
+
+                unsafe { self.head_of_queue = next };
+            }
+        }
+    }
 }
 
 
@@ -214,7 +381,7 @@ mod tests {
         let mut q = MpscQueue::new();
 
         for i in 0..BUFFER_SIZE * 2 {
-            q.enqueue(i);
+            let _ = q.enqueue(i);
         }
 
         for i in 0..BUFFER_SIZE * 2 {
@@ -227,5 +394,15 @@ mod tests {
                 unsafe { assert_eq!(i, (*q.tail_of_queue.load(Ordering::SeqCst)).curr_buffer[index].data.assume_init()); }
             }
         }
+    }
+
+    #[test]
+    fn dequeue() {
+        let mut q = MpscQueue::new();
+
+        assert_eq!(None, q.dequeue());
+        assert_eq!(Ok(()), q.enqueue(42));
+        assert_eq!(Some(42), q.dequeue());
+        assert_eq!(None, q.dequeue());
     }
 }
